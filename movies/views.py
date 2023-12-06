88cb4cbec430516d3a571b30f72d922b15f9e8b8
peyman dodangeh <peymandodangeh@gmail.com>
2023-12-06 10:06:29 +0330

    adding detail template and Exception Handling.

--- movies/views.py
+++ movies/views.py
@@ -1,17 +1,24 @@
-from django.http import HttpResponse
+from django.http import Http404
 from django.shortcuts import render
+
 from .models import Movie
+
+
 # Create your views here.
 
 
 def index(request):
     movies = Movie.objects.all()
-    #SELECT * FROM MOVIES_MOVIE
+    # SELECT * FROM MOVIES_MOVIE
 
     # output = ', '.join([m.title for m in movies])
 
-    return render(request, 'movies/index.html', {'movies' : movies})
+    return render(request, 'movies/index.html', {'movies': movies})
 
-def detail(request , movie_id):
-    return HttpResponse(movie_id)
 
+def detail(request, movie_id):
+    try:
+        movie = Movie.objects.get(pk=movie_id)
+        return render(request, 'movies/detail.html', {'movie': movie})
+    except Movie.DoesNotExist:
+        raise Http404()
