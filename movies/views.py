881557e604b1498dab6f785e7088384e3ae11555
peyman dodangeh <peymandodangeh@gmail.com>
2023-12-06 10:08:59 +0330

    refactor to pythonic, replace with get_object_or_404.

--- movies/views.py
+++ movies/views.py
@@ -1,5 +1,4 @@
-from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Movie
 
@@ -17,8 +16,5 @@ def index(request):
 
 
 def detail(request, movie_id):
-    try:
-        movie = Movie.objects.get(pk=movie_id)
-        return render(request, 'movies/detail.html', {'movie': movie})
-    except Movie.DoesNotExist:
-        raise Http404()
+    movie = get_object_or_404(Movie, pk=movie_id)
+    return render(request, 'movies/detail.html', {'movie': movie})
