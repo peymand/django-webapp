01557cc548d94ba88939c3b928bc5be452f3732f
peyman dodangeh <peymandodangeh@gmail.com>
2023-12-06 09:48:41 +0330

    adding template.

--- movies/views.py
+++ movies/views.py
@@ -1,9 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import Movie
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Hello World!!")
+    movies = Movie.objects.all()
+    #SELECT * FROM MOVIES_MOVIE
+
+    # output = ', '.join([m.title for m in movies])
+
+    return render(request, 'movies/index.html', {'movies' : movies})
+
+def detail(request , movie_id):
+    return HttpResponse(movie_id)
 
